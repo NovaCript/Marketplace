26b1483da3dabd1f1fdecd2177e71da374a3ab24
NovaCript <soltivco@gmail.com>
2026-04-27 13:26:07 +0300

    add: brandList query/result/handler

---
+++ src/Services/Catalog/Catalog.Application/Handlers/BrandHandler/GetBrandsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.Application.Queries.BrandQueries;
+using Catalog.Application.Responses.BrandResponses;
+using Catalog.Domain.Entities;
+using Catalog.Domain.Repository;
+using MediatR;
+
+namespace Catalog.Application.Handlers.BrandHandler;
+
+public class GetBrandsQueryHandler(IBrandRepository brandRepository) : IRequestHandler<GetBrandsQuery, GetBrandsResult>
+{
+    public async Task<GetBrandsResult> Handle(GetBrandsQuery query,
+        CancellationToken cancellationToken)
+    {
+        IEnumerable<Brand> brandList =
+            await brandRepository.GetAllBrandsAsync(
+                cancellationToken: cancellationToken);
+        GetBrandsResult result = new GetBrandsResult(brandList);
+        return result;
+    }
+}
