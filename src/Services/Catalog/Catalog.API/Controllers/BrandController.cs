f1ef7902461e36165425d09b5e07c47a1a958e61
NovaCript <soltivco@gmail.com>
2026-04-28 13:20:37 +0300

    add: new controller GetBrands

---
+++ src/Services/Catalog/Catalog.API/Controllers/BrandController.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.Application.Queries.BrandQueries;
+using Catalog.Application.Responses.BrandResponses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
+
+public class BrandController(IMediator mediator) : BaseController
+{
+    [HttpGet]
+    public async Task<ActionResult<GetBrandsResult>> GetBrands(CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(new GetBrandsQuery(), cancellationToken: cancellationToken);
+        return result;
+    }
+}
