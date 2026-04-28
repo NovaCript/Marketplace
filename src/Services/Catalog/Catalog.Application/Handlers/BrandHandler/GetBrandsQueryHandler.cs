949e9df17cc0d26b56c7358cd02c3943e83713b8
NovaCript <soltivco@gmail.com>
2026-04-28 13:19:36 +0300

    refactor: reformat code style

--- src/Services/Catalog/Catalog.Application/Handlers/BrandHandler/GetBrandsQueryHandler.cs
+++ src/Services/Catalog/Catalog.Application/Handlers/BrandHandler/GetBrandsQueryHandler.cs
@@ -6,14 +6,16 @@ using MediatR;
 
 namespace Catalog.Application.Handlers.BrandHandler;
 
-public class GetBrandsQueryHandler(IBrandRepository brandRepository) : IRequestHandler<GetBrandsQuery, GetBrandsResult>
+public class GetBrandsQueryHandler(
+    IBrandRepository brandRepository
+    ) : IRequestHandler<GetBrandsQuery, GetBrandsResult>
 {
     public async Task<GetBrandsResult> Handle(GetBrandsQuery query,
         CancellationToken cancellationToken)
     {
         IEnumerable<Brand> brandList =
-            await brandRepository.GetAllBrandsAsync(
-                cancellationToken: cancellationToken);
+            await brandRepository
+                .GetAllBrandsAsync(cancellationToken: cancellationToken);
         GetBrandsResult result = new GetBrandsResult(brandList);
         return result;
     }
