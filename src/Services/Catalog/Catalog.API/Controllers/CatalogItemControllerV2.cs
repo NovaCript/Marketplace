c95270ea0249ebc5d1bbc8f09d5e6669bc85c530
NovaCript <novacript.dev@gmail.com>
2026-05-05 13:19:43 +0300

    add: CatalogItemController Pagination v2

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogItemControllerV2.cs
@@ -0,0 +1,27 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace Catalog.API.Controllers;
+
+[ApiVersion("2")]
+[ApiExplorerSettings(GroupName = "v2")]
+[Route("api/v{version:apiVersion}/CatalogItem")]
+public class CatalogItemControllerV2 : BaseController
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(GetCatalogItemsResultV2), (int)HttpStatusCode.OK)]
+    [SwaggerOperation(Tags = new []{ "CatalogItemControllerV2" })]
+    public async Task<ActionResult<GetCatalogItemsResultV2>> GetCatalogItems(
+        CancellationToken cancellationToken,
+        [FromQuery] int pageIndex = 1,
+        [FromQuery] int pageSize = 5)
+    {
+        var query =
+            new GetCatalogItemsQueryV2(
+                PageIndex: pageIndex,
+                PageSize: pageSize);
+        var result = await Mediator.Send(
+            query,
+            cancellationToken: cancellationToken);
+        return Ok(result);
+    }
+}
