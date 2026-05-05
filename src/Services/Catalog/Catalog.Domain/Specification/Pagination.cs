fe603d1ec3a84374e32aa8d8d3a8e269d48151b6
NovaCript <novacript.dev@gmail.com>
2026-05-05 11:42:57 +0300

    add: pagination generic record

---
+++ src/Services/Catalog/Catalog.Domain/Specification/Pagination.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.Domain.Specification;
+
+public record Pagination<T>(
+    int PageIndex,
+    int PageSize,
+    int TotalCount,
+    IReadOnlyList<T> Items
+    ) where T : class;
